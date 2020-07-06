456b55abe7b86e75b816ee170c22737c88aabf56
sholeha2850 <sholeha.18421007@student.ubl.ac.id>
2020-07-05 21:32:02 -0700

    first commit

---
+++ controller/Movie.js
@@ -0,0 +1,53 @@
+const movieModel = require('../model/Movie')
+const { requestResponse } = require('../config')
+const objectId = require('mongoose').Types.ObjectId
+const { deleteImage } = require('../uploadConfig')
+const movie = require('../model/Movie')
+exports.insertMovie = (data) =>
+new Promise((resolve, reject) => {
+  movieModel.create(data)
+  .then(() => resolve(requestResponse.sukses('Berhasil Input Movie')))
+  .catch(() => reject(requestResponse.serverEror))
+})
+
+exports.getAlMovie = () =>
+new Promise((resolve, reject) => {
+  movieModel.find({})
+  .then(movie => resolve(requestResponse.suksesWithData(movie)))
+  .catch(error=> reject(requestResponse.serverError))
+})
+
+exports.getbyId = (id) => 
+new Promise((resolve, reject) => {
+  movieModel.findOne({
+    _id: objectId(id)
+  }).then(movie => resolve(requestResponse.suksesWithData(movie)))
+  .catch(error => reject(requestResponse.serverError))
+})
+
+exports.edit = (data, id, changeImage) => 
+new Promise(async(resolve, reject) => {
+  movieModel.updateOne({
+    _id: objectId(id)
+  }, data)
+  .then(() => {
+    if (changeImage) {
+      deleteImage(data.oldImage)
+    }
+    resolve(requestResponse.sukses('Berhasil Edit Movie'))
+  }).catch(() => reject(requestResponse.serverError))
+})
+
+exports.delete = (id) => 
+new Promise((resolve, reject) => {
+  movieModel.findOne({
+    _id: objectId(id)
+}).then (movie => {
+  movieModel.deleteOne({
+    _id: objectId(id)
+  }).then(() => {
+    deleteImage(movie.image)
+    resolve(requestResponse.sukses('Berhasil Delete Movie'))
+  }).catch(() => reject(requestResponse.serverEror))
+})
+})
